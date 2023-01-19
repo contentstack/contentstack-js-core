132af79310938f757dac58f0df4647e5fa1cae69
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2023-01-19 15:26:27 +0530

    test: :fire: core spec file removed from src folder

--- src/lib/contentstack-core.spec.ts
+++
@@ -1,7 +0,0 @@
-import { contentstackCore } from './contentstack-core';
-
-describe('contentstackCore', () => {
-  it('should work', () => {
-    expect(contentstackCore()).toEqual('contentstack-js-core');
-  });
-});
