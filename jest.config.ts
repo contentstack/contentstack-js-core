8ba88c768bfdb52dd8460f7ef171c957e73ee5a5
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2023-02-03 13:51:21 +0530

    ci: :construction: coverage threshold added

--- jest.config.ts
+++ jest.config.ts
@@ -13,4 +13,12 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
   coverageDirectory: './coverage/packages/contentstack-js-core',
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  }
 };
