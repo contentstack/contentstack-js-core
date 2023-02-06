3acf5f69d2233d1277a01f50f706ec91d33c1cc1
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2023-02-06 14:07:39 +0530

    ci: :bug: jest preset

--- jest.preset.js
+++ jest.preset.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // This is one of the patterns that jest finds by default https://jestjs.io/docs/configuration#testmatch-arraystring
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
