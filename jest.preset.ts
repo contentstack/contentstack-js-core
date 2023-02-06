cfb35ed0449cdfe2b7e7352f2546014293a8c6b1
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2023-02-06 13:36:55 +0530

    ci: :memo: file jest preset added

---
+++ jest.preset.ts
@@ -0,0 +1,13 @@
+export default {
+  // This is one of the patterns that jest finds by default https://jestjs.io/docs/configuration#testmatch-arraystring
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
+  coverageReporters: ['html'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'require', 'default'],
+  },
+};
