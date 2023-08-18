7b772069ff5749345f24babf881898d2e6598d47
harshithad0703 <harshitha.d@contentstack.com>
2023-08-18 20:05:58 +0530

    refactor: added client config in get data params

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,7 +1,12 @@
-import { AxiosInstance } from './types';
+import { AxiosInstance, IHttpClientParams } from './types';
+import { httpClient } from './contentstack-core';
 
-export async function getData(client: AxiosInstance, url: string, data: { [key: string]: string | number } = {}) {
+export async function getData(clientConfig: IHttpClientParams, url: string, data?: any) {
+  const client: AxiosInstance = httpClient(clientConfig);
   try {
+    if (clientConfig.params?.environment) {
+      data.environment = clientConfig.params.environment;
+    }
     const response = await client.get(url, data);
     if (response.data) {
       return response.data;
