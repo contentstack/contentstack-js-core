5beb92f85297a346d24ee7c245531fce7f385e29
sunil-lakshman <104969541+sunil-lakshman@users.noreply.github.com>
2026-02-11 14:38:17 +0530

    fix: fixed url encode issue for special symbols

--- test/param-serializer.spec.ts
+++ test/param-serializer.spec.ts
@@ -21,15 +21,29 @@ describe('serialize', () => {
 
   it('should return query with encode string when passed query param', (done) => {
     const param = serialize({ query: { title: { $in: ['welcome', 'hello'] } } });
-    expect(param).toEqual('&query=%7B%22title%22:%7B%22$in%22:%5B%22welcome%22,%22hello%22%5D%7D%7D');
+    expect(param).toEqual('&query=%7B%22title%22%3A%7B%22%24in%22%3A%5B%22welcome%22%2C%22hello%22%5D%7D%7D');
     done();
   });
 
   it('should return brackets and query with encoded string when passed query param and array value', (done) => {
     const param = serialize({ include: ['reference'], query: { title: { $in: ['welcome', 'hello'] } } });
     expect(param).toEqual(
-      'include%5B%5D=reference&query=%7B%22title%22:%7B%22$in%22:%5B%22welcome%22,%22hello%22%5D%7D%7D'
+      'include%5B%5D=reference&query=%7B%22title%22%3A%7B%22%24in%22%3A%5B%22welcome%22%2C%22hello%22%5D%7D%7D'
     );
+    done();
+  });
+
+  it('should properly encode special characters like ampersand in query values', (done) => {
+    const param = serialize({ query: { url: '/imaging-&-automation' } });
+    // The & should be encoded as %26
+    expect(param).toEqual('&query=%7B%22url%22%3A%22%2Fimaging-%26-automation%22%7D');
+    done();
+  });
+
+  it('should properly encode other URL-special characters in query values', (done) => {
+    const param = serialize({ query: { title: 'test=value&foo=bar?baz' } });
+    // =, &, and ? should all be encoded
+    expect(param).toEqual('&query=%7B%22title%22%3A%22test%3Dvalue%26foo%3Dbar%3Fbaz%22%7D');
     done();
   });
 });
